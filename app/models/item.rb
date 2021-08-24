4396869765d996a3ede25c0c8ebc0ebd672481c6
Kana-Ma <kana.3611@icloud.com>
2021-08-24 23:47:10 +0900

    モデルとテーブルの作成

---
+++ app/models/item.rb
@@ -0,0 +1,25 @@
+class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_cost
+  belongs_to :prefecture
+  belongs_to :shipping_days
+
+  belongs_to :user
+  has_one_attached :image
+
+  validates :name, presence: true
+  validates :text, presence: true
+
+  validates :price, presence: true,
+                    numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :condition_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :shipping_cost_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :shipping_days_id, numericality: { other_than: 1, message: "can't be blank" }
+
+  validates :image, presence: true
+end
