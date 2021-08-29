f03c110582c78c90aa890de957064331914d03a0
Kana-Ma <kana.3611@icloud.com>
2021-08-29 15:53:13 +0900

    presence: trueをwith_optionsでまとめた

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -2,14 +2,17 @@ class OrderAddress
   include ActiveModel::Model
   attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :municipality, :house_number, :building_name, :phone_number, :token
 
-  validates :user_id, presence: true
-  validates :item_id, presence: true
-  validates :postal_code, presence: true, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
+  with_options presence: true do
+    validates :user_id
+    validates :item_id
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
+    validates :municipality
+    validates :house_number
+    validates :phone_number, format: { with: /\A\d{10,11}\z/ }
+    validates :token
+  end
+
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :municipality, presence: true
-  validates :house_number, presence: true
-  validates :phone_number, presence: true, format: { with: /\A\d{10,11}\z/ }
-  validates :token, presence: true
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
