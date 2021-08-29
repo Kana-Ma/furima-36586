a6902bc0b640eee35ad8081c28cc97982530e68d
Kana-Ma <kana.3611@icloud.com>
2021-08-29 15:25:01 +0900

    before_actionにupdateとdestroyを追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :destroy]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
-  before_action :check_self_item, only: :edit
+  before_action :check_self_item, only: [:edit, :update, :destroy]
 
   def index
     @items = Item.order('created_at DESC')
